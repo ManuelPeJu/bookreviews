b2c999dc42c8b59da4cdd72825dfe3d42d9847e9
Manuel Pérez Jurado <perezjuradomanuel@gmail.com>
2024-07-25 21:49:28 +0200

    added child template for focus content

--- index.php
+++ index.php
@@ -2,7 +2,17 @@
     get_header();   
 ?>
 
-<h1>Hola desde el Index.php</h1>
+<h1>Hola desde el Index.php</h1> <!-- in case the main failed to load for some reason-->
+    <main>
+        <?php
+        while (have_posts()) : the_post(); // start the loop and contact the DB of wp
+
+            the_title(); // set the name of the page/post
+            the_content(); // set the content of the page/post
+
+        endwhile; // end the loop
+        ?>
+    </main>
 
 <?php 
     get_footer();
---
+++ page-contenido-centrado.php
@@ -0,0 +1,17 @@
+
+    <?php //Create the name of the child template
+        /*
+            Template Name: Contenido Centrado (No Sidebars)
+        */
+        get_header();
+    ?>
+
+    <main class="container section focus-content center-text">
+        <?php 
+             get_template_part("template-parts/pagina")
+        ?>
+    </main>
+
+    <?php 
+        get_footer(); // get the footer
+    ?>

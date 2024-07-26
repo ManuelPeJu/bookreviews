f7f0d6347ce742b16241c07c90eb485f935f9ae3
Manuel Pérez Jurado <perezjuradomanuel@gmail.com>
2024-07-26 20:31:33 +0200

    css fix for featured images

--- template-parts/pagina.php
+++ template-parts/pagina.php
@@ -1,9 +1,9 @@
 <?php
 
 while(have_posts()) : the_post();
-    the_title("<h1 class='text-center primary-text'>", "</h1>");
+    the_title("<h1 class='center-text primary-text'>", "</h1>");
     if(has_post_thumbnail()) {
-        the_post_thumbnail('full', array('class' => 'featured-image'));
+        the_post_thumbnail('full', array('class' => 'featured-image', 'about'));
     };
     the_content();
 endwhile;

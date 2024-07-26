a0cf86c554165599a7806c19dfa56059805e0286
Manuel Pérez Jurado <perezjuradomanuel@gmail.com>
2024-07-26 21:06:08 +0200

    created custom post type, about us page child, queries where books are called and new tempalte parts for books

---
+++ includes/queries.php
@@ -0,0 +1,41 @@
+<?php 
+
+    function bookreviews_lista_libros($cantidad = -1) {
+
+        ?>
+        <ul>
+            <?php 
+            $args = array(
+                'post_type' => 'bookreviews_libros',
+                'post_per_page' => $cantidad,
+            );
+
+            $libros = new WP_Query($args);
+
+            while($libros->have_post()) {
+                $libros->the_post();
+                ?>
+                    <li class="card">
+                        <?php the_post_thumbnail(); ?>
+                        <div class="content">
+                            <a href="<?php the_permalink(); ?>">
+                                <h3>
+                                    <?php the_title(); ?>
+                                </h3>
+                                <p>
+                                    <?php 
+                                        the_content();
+                                    ?>
+                                </p>
+                            </a>
+                        </div>
+                    </li>
+                <?php
+            }
+            wp_reset_postdata();
+            ?>
+        </ul>
+
+        <?php
+    } 
+?>
---
+++ page-sobre-nosotros.php
@@ -0,0 +1,16 @@
+<?php 
+    /*
+        Template Name: Sobre nosotros
+    */
+        get_header();
+    ?>
+
+    <main class="container section focus-content center-text">
+        <?php 
+            get_template_part("template-parts/pagina");
+        ?>
+    </main>
+
+    <?php 
+        get_footer();
+    ?>

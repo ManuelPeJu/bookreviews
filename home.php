b64a0288227c1b9fc634ff93407dd7d2182cfdd7
Manuel Pérez Jurado <perezjuradomanuel@gmail.com>
2024-07-30 19:54:54 +0200

    home created

---
+++ home.php
@@ -0,0 +1,22 @@
+<?php
+    get_header(); // get the header
+?>
+
+<main class="section container">
+    <ul class="grid-list">
+        <?php
+            while (have_posts()) {
+                the_post();
+                get_template_part("template-parts/blog");
+            }
+        ?>
+    </ul>
+    <?php 
+       the_posts_pagination();
+    ?>
+</main>
+
+
+<?php
+    get_footer(); // get the footer
+?>

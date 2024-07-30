b450df65031a9ffb72a3e5f5700f9a46c8b6f56c
Manuel Pérez Jurado <perezjuradomanuel@gmail.com>
2024-07-30 20:00:38 +0200

    footer and footer css

--- footer.php
+++ footer.php
@@ -1,5 +1,26 @@
-<?php 
-    wp_footer();
+<footer class="footer container">
+    <hr>
+
+    <div class="footer-content">
+        <?php
+
+        $args = array(
+            "theme_location" => "menu-principal", // locate the menu u want to render
+            "container" => "nav", // set the container as a nav tag, default container are a div
+            "container_class" => "main-menu", // set the classname for a css modification
+        );
+
+        wp_nav_menu($args); // this function render a menu taking a associative array as menu location
+
+        ?>
+
+        <p class="copyright">Todos los derechos reservados. <?php echo get_bloginfo("name") . " " . date("Y"); ?></p>
+    </div>
+</footer>
+
+<?php
+wp_footer();
 ?>
 </body>
+
 </html>

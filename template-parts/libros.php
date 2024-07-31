7875ee506e5b3b808edd122af3e7acbd46914e4e
Manuel Pérez Jurado <perezjuradomanuel@gmail.com>
2024-07-31 18:09:40 +0200

    created a template folder for child templates

--- template-parts/libros.php
+++ template-parts/libros.php
@@ -1,14 +1,13 @@
 <?php 
-
     while(have_posts()) : the_post();
         the_title("<h1 class='center-text primary-text'>", "</h1>");
-        if(has_post_thumbnail()){
-            the_post_thumbnail('full', array('class' => 'featured-image'));
+        if(has_post_thumbnail()) {
+            the_post_thumbnail("full", array("class" => "featured-image"));
         }
+
 
 ?>
 
-    <?php 
-
-    the_content();
+        <?php
+        the_content();
     endwhile;

c80d0014bcdb4dc6f1612875b7bc25624100b900
Manuel Pérez Jurado <perezjuradomanuel@gmail.com>
2024-08-07 20:17:37 +0200

    changed little css on front-page and added comments to custom post types

--- single-bookreviews_books.php
+++ single-bookreviews_books.php
@@ -8,6 +8,30 @@
             get_template_part('template-parts/libros');
         ?>
     </section>
+    <div class="comments focus-content center-text content-2">
+        <?php 
+            comment_form();
+            ?>
+
+            <h3 class="center-text primary-text comments">Comentarios</h3>
+            <ul class="comments-list">
+                <?php 
+                    $comentarios = get_comments(array(
+                        'post_id' => $post -> ID,
+                        'status' => 'approve',
+
+                    ));
+                    wp_list_comments(array(
+                        'per_page' => 10,
+                        'reverse_top_level' => false,
+                    ), $comentarios)
+                    
+                ?>
+            </ul>
+
+            <?php
+        ?>
+    </div>
 </main>
 
 <?php 
